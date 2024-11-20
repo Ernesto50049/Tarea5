83324b381b86f31bfd7f54ddf5dae8d28573f3cc
Ernesto50049 <angelernestoabril@gmail.com>
2024-11-20 15:14:06 -0700

    Add files via upload

---
+++ datos.sql
@@ -0,0 +1,28 @@
+CREATE TABLE articulos (
+    id INT PRIMARY KEY,
+    nombre VARCHAR (100),
+    precio DECIMAL (10,2),
+    cantidad_en_stock INT
+);
+
+INSERT INTO articulos (nombre, precio, cantidad_en_stock) VALUES
+("Arroz Blanco", 15.99, 100),
+("Harina de trigo", 15.99, 3),
+("Azúcar blanca", 15.99, 5),
+("Aceite de oliva", 15.99, 7),
+("Sal de mesa", 15.99, 9),
+("Leche entera", 15.99, 2),
+("Mantequilla", 15.99, 9),
+("Cereal de avena", 15.99, 1),
+("Pasta espageti", 15.99, 100),
+("Tomate enlatados", 15.99, 100),
+("Frijoles negros", 15.99, 100),
+("Salsa de tomate ", 15.99, 100),
+("Galletas oreo", 15.99, 100),
+("Queso chedar rallado", 15.99, 100),
+("Pollo congelado", 15.99, 100),
+("Carne molida de res", 15.99, 100),
+("Jugo de naranja", 15.99, 100),
+("Manzanas", 15.99, 100),
+("Plátanos", 15.99, 100),
+("Papitas fritas", 15.99, 100),
